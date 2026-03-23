36e79703881ec248cd76e82ff9f6e0294fff2702
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-03-23 12:53:26 -0300

    add- tentar fazer em casa

--- scripts/ex2.js
+++ scripts/ex2.js
@@ -1,5 +1,19 @@
 function NasaFoguete() {
-    for(let i = 10; i < 0; i++){
-        
-    }
+
+    // Definimos o tempo inicial
+    let segundos = 10;
+    
+    // Criamos o intervalo que roda a cada 1 segundo (1000ms)
+    const contagem = setInterval(),{
+
+        console.log(segundos); // Exibe o número atual no console
+      
+        segundos--; // Subtrai 1 do valor
+      
+        // Quando chegar a zero, paramos o intervalo
+        if (segundos < 0) 
+          clearInterval(contagem);
+          console.log("Tempo esgotado!");
+    } 
 }
+  
