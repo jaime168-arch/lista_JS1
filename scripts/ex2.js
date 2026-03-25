c46b659efb50ea30af9a555d663f50a4ba665973
jaime_r_pereira <jaime_r_pereira@estudante.sesisenai.org.br>
2026-03-25 10:12:52 -0300

    add- novos exercicios e atividade 2 concluida

--- scripts/ex2.js
+++ scripts/ex2.js
@@ -1,19 +1,11 @@
 function NasaFoguete() {
 
-    // Definimos o tempo inicial
-    let segundos = 10;
-    
-    // Criamos o intervalo que roda a cada 1 segundo (1000ms)
-    const contagem = setInterval(),{
-
-        console.log(segundos); // Exibe o número atual no console
-      
-        segundos--; // Subtrai 1 do valor
-      
-        // Quando chegar a zero, paramos o intervalo
-        if (segundos < 0) 
-          clearInterval(contagem);
-          console.log("Tempo esgotado!");
-    } 
+ for (let i = 10; i > 0; i--) {
+    console.log(i);
 }
-  
+
+    
+    alert("Foguete lançado");
+    
+    
+}
